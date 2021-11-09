ecd0632ec744d6677b7b45b8d4a6cd8aad166964
IsraelBosonit <92779616+IsraelBosonit@users.noreply.github.com>
2021-11-09 14:38:59 +0100

    commit

---
+++ DB1/src/main/java/com/example/DB1/Persona/application/port/PersonaRepositorio.java
@@ -0,0 +1,12 @@
+package com.example.DB1.Persona.application.port;
+
+import com.example.DB1.Persona.domain.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PersonaRepositorio extends JpaRepository<Persona, String> {
+
+    List<Persona> findByusuario(String usuario);
+}
+
